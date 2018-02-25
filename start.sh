e11d5c0c3b9fec123ad04245772718de1d734533
June Tate-Gans <june@theonelab.com>
2018-02-25 15:55:12 -0800

    start.sh: Be verbose and copy server.properties from configmap
    This makes start.sh copy any ConfigMap'ed server.properties into the minecraft
    data directory, and adds a bunch of verbosity to the script so that when one
    read logs, it's clear what's going on instead of running into "nothing" until
    minecraft starts.

--- start.sh
+++ start.sh
@@ -3,12 +3,21 @@
 set -e
 
 cd /data
+echo "preparing /data with modpack"
 cp -vrf /tmp/feed-the-beast/* .
 echo "eula=true" > eula.txt
 
+if [[ -e /properties/server.properties ]] && [[ ! -e /data/server.properties ]]; then
+    echo "/data/server.properties not present and /properties/server.properties present."
+    echo "copying server.properties"
+    cp /properties/server.properties /data/server.properties
+fi
+
 if [[ ! -z "${RCON_PASSWORD}" ]] && ! grep -q rcon.password server.properties 2>/dev/null; then
+    echo "enabling RCON, since RCON_PASSWORD is set and not present in server.properties"
     echo "rcon.password=${RCON_PASSWORD}" >> server.properties
     sed -i -e 's/enable-rcon=false/enable-rcon=true/' server.properties
 fi
 
+echo "starting minecraft"
 java $JVM_OPTS -jar FTBserver-*.jar nogui
